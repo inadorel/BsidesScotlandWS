5630e8b2b101fae3eab818d36a7e11b40d16173c
marionm <marion.mccune@scotsts.com>
2017-06-08 21:03:30 +0100

    Got the sponsors part to work.

---
+++ BsidesScotlandWS/Scripts/app.js
@@ -0,0 +1,39 @@
+﻿var ViewModel = function () {
+    var self = this;
+    self.sponsors = ko.observableArray();
+    self.error = ko.observable();
+    self.detail = ko.observable();
+
+    var sponsorsUri = '/api/sponsors/';
+
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+
+    function getAllSponsors() {
+        ajaxHelper(sponsorsUri, 'GET').done(function (data) {
+            self.sponsors(data);
+        });
+    }
+
+    
+    self.getSponsorDetail = function (item) {
+        ajaxHelper(sponsorsUri + item.SponsorId, 'GET').done(function (data) {
+            self.detail(data);
+        });
+    }
+
+    // Fetch the initial data.
+    getAllSponsors();
+};
+
+ko.applyBindings(new ViewModel());
